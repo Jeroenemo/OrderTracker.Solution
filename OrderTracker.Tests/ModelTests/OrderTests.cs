3da3e07ac353e2a88d01aa7b62818110840e21aa
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-03-05 09:01:36 -0800

    test for order constructor

---
+++ OrderTracker.Tests/ModelTests/OrderTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using OrderTracker.Models;
+using System;
+
+namespace OrderTracker.Tests
+{
+  [TestClass]
+  public class OrderTest
+  {
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    {
+      Order newOrder = new Order("TestTitle", "TestDescription", 1.99);
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+  }
+}
---
+++ OrderTracker/Models/Order.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+
+namespace OrderTracker.Models
+{
+  public class Order
+  {
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public double Price { get; set; }
+    public DateTime Date { get; set; }
+    public int Id { get; }
+
+    private static List<Order> _instances = new List<Order> {};
+
+    public Order(string title, string description, double price)
+    {
+      Title = title;
+      Description = description;
+      Price = price;
+      Date = DateTime.Today;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
+  }
+}
