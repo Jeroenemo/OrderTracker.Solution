d8901eee38fefea918a896b8f24531235debe6c8
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-03-05 11:29:48 -0800

    add vendors route

---
+++ OrderTracker/Controllers/VendorsController.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using OrderTracker.Models;
+
+namespace OrderTracker.Controllers
+{
+  public class VendorsController : Controller
+  {
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
+    }
+  }
+}
