b77c8370dfddffd790021548fcedcfa13ea3a347
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-03-05 09:10:33 -0800

    test to return order properties

--- OrderTracker.Tests/ModelTests/OrderTests.cs
+++ OrderTracker.Tests/ModelTests/OrderTests.cs
@@ -14,5 +14,21 @@ namespace OrderTracker.Tests
       Order newOrder = new Order("TestTitle", "TestDescription", 1.99);
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
+    [TestMethod]
+    public void GetProperties_ReturnsProperties_String_String_Double()
+    {
+      string title = "New Order";
+      string description = "Order Description";
+      double price = 19.99;
+
+      Order newOrder = new Order(title, description, price);
+      string resultTitle = newOrder.Title;
+      string resultDescription = newOrder.Description;
+      double resultPrice = newOrder.Price;
+      
+      Assert.AreEqual(title, resultTitle);
+      Assert.AreEqual(description, resultDescription);
+      Assert.AreEqual(price, resultPrice);
+    }
   }
 }
