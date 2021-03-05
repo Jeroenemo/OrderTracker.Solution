7b222b268b7850800b448776ec495f2c90f0de88
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-03-05 09:16:25 -0800

    test for updating order properties

--- OrderTracker.Tests/ModelTests/OrderTests.cs
+++ OrderTracker.Tests/ModelTests/OrderTests.cs
@@ -25,10 +25,32 @@ namespace OrderTracker.Tests
       string resultTitle = newOrder.Title;
       string resultDescription = newOrder.Description;
       double resultPrice = newOrder.Price;
-      
+
       Assert.AreEqual(title, resultTitle);
       Assert.AreEqual(description, resultDescription);
       Assert.AreEqual(price, resultPrice);
+    }
+    [TestMethod]
+    public void SetProperties_SetProperties_String_String_Double()
+    {
+      string title = "New Order";
+      string description = "Order Description";
+      double price = 19.99;
+      Order newOrder = new Order(title, description, price);
+
+      string updatedTitle = "Newer Order";
+      string updatedDescription = "New Order Description";
+      double updatedPrice = 20.00;
+      newOrder.Title = updatedTitle;
+      newOrder.Description = updatedDescription;
+      newOrder.Price = updatedPrice;
+      string resultTitle = newOrder.Title;
+      string resultDescription = newOrder.Description;
+      double resultPrice = newOrder.Price;
+      
+      Assert.AreEqual(updatedTitle, resultTitle);
+      Assert.AreEqual(updatedDescription, resultDescription);
+      Assert.AreEqual(updatedPrice, resultPrice);
     }
   }
 }
