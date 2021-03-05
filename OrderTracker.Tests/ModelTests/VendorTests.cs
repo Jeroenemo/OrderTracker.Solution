f96b8aeb4a90a89dc182a02ac8b748c2e7b24259
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-03-05 10:12:53 -0800

    test to return vendor name

--- OrderTracker.Tests/ModelTests/VendorTests.cs
+++ OrderTracker.Tests/ModelTests/VendorTests.cs
@@ -18,5 +18,18 @@ namespace OrderTracker.Models
       Vendor newVendor = new Vendor("test vendor");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
+    [TestMethod]
+    public void GetName_ReturnsName_String()
+    {
+      //Arrange
+      string name = "test vendor";
+      Vendor newVendor = new Vendor(name);
+
+      //Act
+      string result = newVendor.Name;
+
+      //Assert
+      Assert.AreEqual(name, result);
+    }
   }
 }
