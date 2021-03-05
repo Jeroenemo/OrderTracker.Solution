b623fab00881d56a307593f4a4a72fcbb6523c8a
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-03-05 09:38:42 -0800

    test to return populated list

--- OrderTracker.Tests/ModelTests/OrderTests.cs
+++ OrderTracker.Tests/ModelTests/OrderTests.cs
@@ -21,15 +21,18 @@ namespace OrderTracker.Tests
     [TestMethod]
     public void GetProperties_ReturnsProperties_String_String_Double()
     {
+      // Arrange
       string title = "New Order";
       string description = "Order Description";
       double price = 19.99;
 
+      // Act
       Order newOrder = new Order(title, description, price);
       string resultTitle = newOrder.Title;
       string resultDescription = newOrder.Description;
       double resultPrice = newOrder.Price;
 
+      // Assert
       Assert.AreEqual(title, resultTitle);
       Assert.AreEqual(description, resultDescription);
       Assert.AreEqual(price, resultPrice);
@@ -37,11 +40,13 @@ namespace OrderTracker.Tests
     [TestMethod]
     public void SetProperties_SetProperties_String_String_Double()
     {
+      // Arrange
       string title = "New Order";
       string description = "Order Description";
       double price = 19.99;
       Order newOrder = new Order(title, description, price);
 
+      // Act
       string updatedTitle = "Newer Order";
       string updatedDescription = "New Order Description";
       double updatedPrice = 20.00;
@@ -52,6 +57,7 @@ namespace OrderTracker.Tests
       string resultDescription = newOrder.Description;
       double resultPrice = newOrder.Price;
       
+      // Assert
       Assert.AreEqual(updatedTitle, resultTitle);
       Assert.AreEqual(updatedDescription, resultDescription);
       Assert.AreEqual(updatedPrice, resultPrice);
@@ -66,6 +72,26 @@ namespace OrderTracker.Tests
       List<Order> result = Order.GetAll();
 
       // Assert
+      CollectionAssert.AreEqual(newList, result);
+    }
+    [TestMethod]
+    public void GetAll_ReturnsOrders_OrderList()
+    {
+      //Arrange
+      string title01 = "title1";
+      string title02 = "title2";
+      string description01 = "an order";
+      string description02 = "another order";
+      double price01 = 34.54;
+      double price02 = 21.21;
+      Order newOrder1 = new Order(title01, description01, price01);
+      Order newOrder2 = new Order(title02, description02, price02);
+      List<Order> newList = new List<Order> { newOrder1, newOrder2 };
+
+      //Act
+      List<Order> result = Order.GetAll();
+
+      //Assert
       CollectionAssert.AreEqual(newList, result);
     }
   }
